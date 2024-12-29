73aef85af79abb7dc21aaf05877ad7a8d92f85f8
Mawuliah Mansaray <mawuliahlmansaray@gmail.com>
2024-12-29 12:35:03 +0000

    adding the vercel deployment

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NODE_ENV === 'production' 
   ? 'https://vsla-backend.vercel.app/api'
-  : '/api'; 
+  : '/api';  // This will work with the Vite proxy in development
