6eb5ff5d6555e38c7fc334d1df007151c74812f3
Mawuliah Mansaray <mawuliahlmansaray@gmail.com>
2024-12-29 14:11:08 +0000

    backend

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,8 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:5174',
-    'https://vsla.vercel.app'
+    'https://vsla.vercel.app',
+    'https://vsla-ah2xqsovn-khat19s-projects.vercel.app'
   ],
   credentials: true
 }));
